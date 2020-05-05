35bfd24cb137feb3148ff3815b63b259cf12f2c0
Pawel Bak <pawelbak300589@gmail.com>
2020-05-05 12:14:36 +0100

    React course - 17 Handling Authentication with React - 217 On-Demand Sign In and Sign Out

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '1000598842672-eqidv4nc75378fe00j7bvkdnjl8l2697.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button" onClick={this.onSignOutClick}>
+                    <i className="google icon"></i>
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button className="ui red google button" onClick={this.onSignInClick}>
+                    <i className="google icon"></i>
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="item">
+                { this.renderAuthButton() }
+            </div>
+        );
+    }
+}
+
+export default GoogleAuth;
